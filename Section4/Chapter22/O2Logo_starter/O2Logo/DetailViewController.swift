ba65fa2e18e983b9d266df279106c1d6ab01ae37
linyi31 <linyi@jd.com>
2016-03-21 19:53:24 +0800

    add: starter project of Chapter22

---
+++ Section4/Chapter22/O2Logo_starter/O2Logo/DetailViewController.swift
@@ -0,0 +1,57 @@
+//
+//  DetailViewController.swift
+//  O2Logo
+//
+//  Created by O2.LinYi on 16/3/21.
+//  Copyright © 2016年 jd.com. All rights reserved.
+//
+
+import UIKit
+import QuartzCore
+
+class DetailViewController: UITableViewController, UINavigationControllerDelegate {
+    
+    let maskLayer: CAShapeLayer = RWLogoLayer.logoLayer()
+    
+    // MARK: - Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Pack List"
+        tableView.rowHeight = 54
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+    }
+
+    // MARK: - Table view data source
+    
+    let packItems = ["Icecream money", "Great weather", "Beach ball", "Swim suit for him", "Swim suit for her", "Beach games", "Ironing board", "Cocktail mood", "Sunglasses", "Flip flops"]
+    
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 10
+    }
+
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+
+        let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath:  indexPath) as UITableViewCell
+        
+        cell.accessoryType = .None
+        cell.textLabel!.text = packItems[indexPath.row]
+        cell.imageView!.image = UIImage(named: "summericons_100px_0\(indexPath.row).png")
+        return cell
+    }
+    
+
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        tableView.deselectRowAtIndexPath(indexPath, animated: true)
+    }
+
+}
